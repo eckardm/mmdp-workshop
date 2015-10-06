554a1a74b99f1f9c52b3f9e3e102fccf77bc2523
Max Eckard <eckardm@umich.edu>
2015-10-06 09:15:10 -0400

    final script and many, many comments

--- get_subjects-FINAL.py
+++ get_subjects-FINAL.py
@@ -11,6 +11,10 @@ from os.path import join
 # this blog post might help out: http://archival-integration.blogspot.com/2015/10/tools-for-programming-archivist-ead.html
 from lxml import etree
 
+# implements classes to read and write tabular (read table-like) data in csv format
+# it already comes with python
+import csv
+
 
 '''
 preliminaries'''
@@ -20,14 +24,41 @@ preliminaries'''
 # if it's not, you'll need to write out the absolute path
 path = 'EADs'
 
+
+'''
+loop through the files and the subjects in the file'''
+
 # loop through each filename in the eads folder
 for filename in os.listdir(path):
     # create an etree or element tree object out of it so that lxml can work with it
     tree = etree.parse(join(path, filename))
-    # perform an xpath search for subject sub-elements of controlaccess elements
+    
+    # perform a general xpath search for subject sub-elements of controlaccess elements
     # note, this returns a list of all subjects in the ead
     subjects = tree.xpath('//controlaccess/subject')
     
     # because it returns a list of all subjects in the ead, we'll need to loop through each subject in that list
     for subject in subjects:
-        
+    
+    
+            '''
+            create a couple variables that we'll need, noting that we already have one for filename'''
+            
+            # particular xpath
+            subject_xpath = tree.getpath(subject)
+            # text
+            subject_text = subject.text.encode('utf-8')
+            
+            
+            '''
+            write it to a csv'''
+            
+            # open up a csv file (the actual file is subjects.csv, but we'll be using a variable called csv_file in the code) in append mode (the a in ab)
+            # note, the b in ab means binary, and for all practical purposes just means that our csv will be single-spaced instead of double-spaced
+            with open('subjects.csv', 'ab') as csv_file:
+                # create a writer (just something we have to do when using the csv module)
+                writer = csv.writer(csv_file)
+                # user the writer to write rows of the variables that we'll need
+                # note, this function takes a list, and in python lists are enclosed in square brackets
+                writer.writerow([filename, subject_xpath, subject_text])
+            
